bbeb52d6cb3c17acd20247aca8eaf8b129d38df6
adferrin <adferrin@icloud.com>
2021-02-25 12:40:30 -0800

    setting up cal functions

--- app.js
+++ app.js
@@ -2,8 +2,33 @@
 const hourEL = document.querySelector('.hour');
 const minuteEL = document.querySelector('.minute');
 
+const displayEl = document.querySelector('.display');
 
+const acEl = document.querySelector('.ac');
+const pmEl = document.querySelector('.pm');
+const percentEl = document.querySelector('.percent');
 
+const aditionEl = document.querySelector('.addition');
+const subtractionEl = document.querySelector('.subtraction');
+const multiplicationEl = document.querySelector('.multiplication');
+const divisionEl = document.querySelector('.division');
+const equalEl = document.querySelector('.equal');
+
+const decimalEl = document.querySelector('.decimal');
+const number0El = document.querySelector('.number-0');
+const number1El = document.querySelector('.number-1');
+const number2El = document.querySelector('.number-2');
+const number3El = document.querySelector('.number-3');
+const number4El = document.querySelector('.number-4');
+const number5El = document.querySelector('.number-5');
+const number6El = document.querySelector('.number-6');
+const number7El = document.querySelector('.number-7');
+const number8El = document.querySelector('.number-8');
+const number9El = document.querySelector('.number-9');
+const numberElArray = [
+    number0El, number1El, number2El, number3El, number4El, number5El,
+    number6El, number7El, number8El, number9El
+];
 
 //Set up time
 const updateTime = () => {
